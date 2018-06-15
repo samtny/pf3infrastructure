bce9f572186cf0d26193a67d660578921c268b18
Sam Thompson <sthompson@digitalpulp.com>
2018-06-15 13:15:31 -0400

    vpcs-get

--- vpcs-get.sh
+++ vpcs-get.sh
@@ -20,11 +20,14 @@ DESCRIBE_VPCS=$(aws ec2 describe-vpcs --output text --filters Name=tag:Name,Valu
 VPC_ID=$(echo "$DESCRIBE_VPCS" | grep "^VPCS" | cut -f7)
 echo -e "aws_VpcId:\t$VPC_ID"
 
-SUBNET_ID_PRIVATE=$(aws ec2 describe-subnets --output text --filters Name=tag:Name,Values=$PROJECT-private | grep "^SUBNETS" | cut -f8)
+SUBNET_ID_PRIVATE=$(aws ec2 describe-subnets --output text --filters Name=tag:Name,Values=$PROJECT-private | grep "^SUBNETS" | cut -f9)
 echo -e "aws_SubnetIdPrivate:\t$SUBNET_ID_PRIVATE"
 
-SUBNET_ID_PUBLIC=$(aws ec2 describe-subnets --output text --filters Name=tag:Name,Values=$PROJECT-public | grep "^SUBNETS" | cut -f8)
+SUBNET_ID_PUBLIC=$(aws ec2 describe-subnets --output text --filters Name=tag:Name,Values=$PROJECT-public | grep "^SUBNETS" | cut -f9)
 echo -e "aws_SubnetIdPublic:\t$SUBNET_ID_PUBLIC"
+
+SUBNET_ID_PUBLIC_ALT=$(aws ec2 describe-subnets --output text --filters Name=tag:Name,Values=$PROJECT-public-alt | grep "^SUBNETS" | cut -f9)
+echo -e "aws_SubnetIdPublicAlt:\t$SUBNET_ID_PUBLIC_ALT"
 
 SECURITY_GROUP_ID_ELB=$(aws ec2 describe-security-groups --output text --filters Name=vpc-id,Values=$VPC_ID Name=tag:Name,Values=$PROJECT-elb | grep "^SECURITYGROUPS" | cut -f3)
 echo -e "aws_SecurityGroupIdElb:\t$SECURITY_GROUP_ID_ELB"
